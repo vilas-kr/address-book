7e5bfae30e9f8937f4240bcf0a4949017d54adb6
vilas <vilaskr2962@gmail.com>
2025-12-10 12:22:55 +0530

    Contact creation done

--- src/main/java/com/bridgelabs/AddressBook.java
+++ src/main/java/com/bridgelabs/AddressBook.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 @Getter
 @Setter
@@ -16,4 +17,39 @@ public class AddressBook {
         contacts = new ArrayList<Contact>();
     }
 
+    public Contact addContact(){
+
+        Scanner scanner = new Scanner(System.in);
+        Contact contact = new Contact();
+
+        //Fetching details
+        System.out.print("Enter First Name : ");
+        contact.setFirstName(scanner.nextLine());
+
+        System.out.print("Enter Last Name : ");
+        contact.setLastName(scanner.nextLine());
+
+        System.out.print("Enter Area : ");
+        contact.setArea(scanner.nextLine());
+
+        System.out.print("Enter City : ");
+        contact.setCity(scanner.nextLine());
+
+        System.out.print("Enter State : ");
+        contact.setState(scanner.nextLine());
+
+        System.out.print("Enter Pincode : ");
+        contact.setZip(scanner.nextInt());
+
+        System.out.print("Enter Phone Number : ");
+        contact.setPhoneNumber(scanner.nextLong());
+        scanner.nextLine();
+
+        System.out.print("Enter Email : ");
+        contact.setEmail(scanner.nextLine());
+
+        contacts.add(contact);
+        return contact;
+
+    }
 }
--- src/main/java/com/bridgelabs/Main.java
+++ src/main/java/com/bridgelabs/Main.java
@@ -4,6 +4,13 @@ package com.bridgelabs;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello and welcome!");
+
+        AddressBook book = new AddressBook();
+
+        Contact contact = book.addContact();
+        System.out.print(contact);
+
     }
 }
